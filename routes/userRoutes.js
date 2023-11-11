47c372eae349296228a1cd3ada0c803be7478418
vankuver163 <de1uxe87@mail.ru>
2023-11-11 10:55:03 +0400

    Controllers

---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const usersController = require('../controllers/usersController')
+
+router.route('/')
+    .get(usersController.getAllUsers)
+    .post(usersController.createNewUser)
+    .patch(usersController.updatedUser)
+    .delete(usersController.deleteUser)
+
+module.exports = router
