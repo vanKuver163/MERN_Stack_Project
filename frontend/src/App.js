bb39e0085df141595aa02f08e8a3a83850466a45
vankuver163 <de1uxe87@mail.ru>
2023-11-13 14:10:35 +0400

    Some components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Public from './components/Public';
 import Login from './features/auth/Login';
 import DashLayoute from './components/DashLayoute';
+import Welcome from './features/auth/Welcome';
+import NotesList from './features/notes/NotesList';
+import UsersList from './features/users/UsersList';
+
 
 function App() {
   return (
@@ -12,7 +14,21 @@ function App() {
       <Route path='/' element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
-        <Route path="dash" element={<DashLayoute />} />
+        <Route path="dash" element={<DashLayoute />}>
+
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+
+        </Route>{/* End Dash */}
+
+      
       </Route>
     </Routes>
   );
--- frontend/src/components/DashFooter.js
+++ frontend/src/components/DashFooter.js
@@ -1,24 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHouse } from "@fortawesome/free-solid-svg-icons"
-import { useNavigate, useLocation } from "react-router-dom"
+import { useNavigate, useLocation } from 'react-router-dom'
 
 const DashFooter = () => {
-    const navigate = useNavigate();
-    const { pathname } = useLocation();
-    const onGoHomeClicked = navigate('/dash');
 
-    let goHomeButton = null;
+    const navigate = useNavigate()
+    const { pathname } = useLocation()
 
+    const onGoHomeClicked = () => navigate('/dash')
+
+    let goHomeButton = null
     if (pathname !== '/dash') {
         goHomeButton = (
-            <button className="dash-footer__button icon-button" title="Home" onClick={onGoHomeClicked}>
+            <button
+                className="dash-footer__button icon-button"
+                title="Home"
+                onClick={onGoHomeClicked}
+            >
                 <FontAwesomeIcon icon={faHouse} />
             </button>
         )
     }
 
     const content = (
-        <footer className='dash-footer'>
+        <footer className="dash-footer">
             {goHomeButton}
             <p>Current User:</p>
             <p>Status:</p>
@@ -26,5 +31,5 @@ const DashFooter = () => {
     )
     return content
 }
-
 export default DashFooter
+
