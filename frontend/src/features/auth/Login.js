ca7990a91162214ad3f1e932e1dfc882687f815b
vankuver163 <de1uxe87@mail.ru>
2023-12-05 17:17:18 +0400

    authApi

--- frontend/src/features/auth/Login.js
+++ frontend/src/features/auth/Login.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import { useRef, useState, useEffect } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+
+import { useDispatch } from 'react-redux'
+import { setCredentials } from './authSlice'
+import { useLoginMutation } from './authApi'
 
 const Login = () => {
+  const userRef = useRef()
+  const errRef = useRef()
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+
+  const navigate = useNavigate();
+  const dispath = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation()
+
+  const errClass = errMsg ? "errmsg" : "offscreen"
+
+  if (isLoading) return <p>Loading...</p>
+
   return (
     <h1>Login</h1>
   )
