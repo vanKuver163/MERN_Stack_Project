54903bc7a161ea6e37714123c7fc62619af0d32a
vankuver163 <de1uxe87@mail.ru>
2023-12-25 16:53:40 +0400

    DashHeader

--- frontend/src/features/auth/Login.js
+++ frontend/src/features/auth/Login.js
@@ -6,93 +6,94 @@ import { setCredentials } from './authSlice'
 import { useLoginMutation } from './authApi'
 
 const Login = () => {
-  const userRef = useRef()
-  const errRef = useRef()
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errMsg, setErrMsg] = useState('');
+    const userRef = useRef()
+    const errRef = useRef()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [errMsg, setErrMsg] = useState('')
 
-  const navigate = useNavigate();
-  const dispath = useDispatch();
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
 
-  const [login, { isLoading }] = useLoginMutation()
+    const [login, { isLoading }] = useLoginMutation()
 
-  useEffect(() => {
-    userRef.current.focus()
-  }, [])
+    useEffect(() => {
+        userRef.current.focus()
+    }, [])
 
-  useEffect(() => {
-    setErrMsg('')
-  }, [username, password])
+    useEffect(() => {
+        setErrMsg('');
+    }, [username, password])
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    try {
-        const {accessToken} = await login ({username, password}).unwrap()
-        dispath(setCredentials({accessToken}))
-        setUsername('')
-        setPassword('')
-        navigate('/dash')
-    } catch (err) {
-        if (!err.status) {
-          setErrMsg('No server Response');
-        } else if (err.status === 400) {
-          setErrMsg('Missing Username or Password');
-        } else if (err.status === 401) {
-          setErrMsg('Unauthorized');
-        } else {
-          setErrMsg(err.data?.message)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const { accessToken } = await login({ username, password }).unwrap()
+            dispatch(setCredentials({ accessToken }))
+            setUsername('')
+            setPassword('')
+            navigate('/dash')
+        } catch (err) {
+            if (!err.status) {
+                setErrMsg('No Server Response');
+            } else if (err.status === 400) {
+                setErrMsg('Missing Username or Password');
+            } else if (err.status === 401) {
+                setErrMsg('Unauthorized');
+            } else {
+                setErrMsg(err.data?.message);
+            }
+            errRef.current.focus();
         }
-        errRef.current.focus();
     }
-  }
 
-  const handleUserInput = (e) => setUsername(e.target.value)
-  const handlePwdInput = (e) => setUsername(e.target.value)
+    const handleUserInput = (e) => setUsername(e.target.value)
+    const handlePwdInput = (e) => setPassword(e.target.value)
 
-  const errClass = errMsg ? "errmsg" : "offscreen"
+    const errClass = errMsg ? "errmsg" : "offscreen"
 
-  if (isLoading) return <p>Loading...</p>
+    if (isLoading) return <p>Loading...</p>
 
-  const content = (
-    <section className='public'>
-      <header>
-        <h1>Employee Login</h1>
-      </header>
-      <main className='login'>
-        <p ref={errRef} className={errClass} aria-live='assertive'>{errMsg}</p>
+    const content = (
+        <section className="public">
+            <header>
+                <h1>Employee Login</h1>
+            </header>
+            <main className="login">
+                <p ref={errRef} className={errClass} aria-live="assertive">{errMsg}</p>
 
-        <form className='form' onSubmit={handleSubmit}>
-          <label htmlFor='username'>Username:</label>
-          <input
-              className='form__input'
-              type="text"
-              id="username"
-              ref={userRef}
-              value={username}
-              onChange={handleUserInput}
-              autoComplete="off"
-              required
-          />
-          <label htmlFor='username'>Password:</label>
-          <input
-              className='form__input'
-              type="password"
-              id="password"              
-              value={password}
-              onChange={handlePwdInput}            
-              required
-          />
-          <button className='form__submit-button'>Sign In</button>
-        </form>
-      </main>
-      <footer>
-        <Link to="/">Back to Home</Link>
-      </footer>
-    </section>
-  )
+                <form className="form" onSubmit={handleSubmit}>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        className="form__input"
+                        type="text"
+                        id="username"
+                        ref={userRef}
+                        value={username}
+                        onChange={handleUserInput}
+                        autoComplete="off"
+                        required
+                    />
 
-  return content
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        className="form__input"
+                        type="password"
+                        id="password"
+                        onChange={handlePwdInput}
+                        value={password}
+                        required
+                    />
+                    <button className="form__submit-button">Sign In</button>
+                </form>
+            </main>
+            <footer>
+                <Link to="/">Back to Home</Link>
+            </footer>
+        </section>
+    )
+
+    return content
 }
-
 export default Login
