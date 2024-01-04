3f76556f5ceb05bce95bd8b77662949a796a648e
vankuver163 <de1uxe87@mail.ru>
2024-01-04 14:48:50 +0400

    useAuth

--- frontend/src/components/DashFooter.js
+++ frontend/src/components/DashFooter.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHouse } from "@fortawesome/free-solid-svg-icons"
 import { useNavigate, useLocation } from 'react-router-dom'
+import useAuth from "../hooks/useAuth"
 
 const DashFooter = () => {
 
