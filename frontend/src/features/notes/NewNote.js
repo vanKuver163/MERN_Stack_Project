dd2b9512715a47f6172bca492921d9cf0a0a019d
vankuver163 <de1uxe87@mail.ru>
2024-01-05 11:03:03 +0400

    EditNote & NewNote

--- frontend/src/features/notes/NewNote.js
+++ frontend/src/features/notes/NewNote.js
@@ -1,12 +1,20 @@
-import { useSelector } from 'react-redux'
-import { selectAllUsers } from '../users/usersApiSlice'
 import NewNoteForm from './NewNoteForm'
+import { useGetUsersQuery } from '../users/usersApiSlice'
+import { PulseLoader } from 'react-spinners'
 
 
 const NewNote = () => {
-    const users = useSelector(selectAllUsers)
-    if(!users?.length) return <p>Not Currently Available</p>
-    const content = <NewNoteForm users={users} /> 
+
+    const { users } = useGetUsersQuery("usersList", {
+        selectFromResult: ({ data }) => ({
+            users: data?.ids.map(id => data?.entities[id])
+        }),
+    })
+
+    if (!users?.length) return <PulseLoader color={"#FFF"} />
+
+    const content = <NewNoteForm users={users} />
+    
     return content
 }
 
